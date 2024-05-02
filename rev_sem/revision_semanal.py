963c463fe8a881908bd91e1600f5d17156247553
jose <jose.s.contacto@gmail.com>
2024-05-02 14:09:04 -0300

    wip(actions) create waitable step decorator

--- rev_sem/revision_semanal.py
+++ rev_sem/revision_semanal.py
@@ -3,23 +3,37 @@ from system import system
 ## TODO: Hacer que exista una lista de funciones y por cada una el iniciar las llame y formatee con un espacio y espaciado final
 
 
-
 class Jobs:
 
     def __init__(self,system=None):
         self._system=system
 
-    def espaciado(self):
+    @staticmethod
+    def espaciado():
         os.system("clear")  
         print("#=======================================#")
         print("")
 
-    def espaciado_final(self):
+    @staticmethod
+    def espaciado_final():
         print("")
         print("-----------------------------------------")
 
+    @staticmethod
+    def esperar():
+        input("Terminaste? s")
+
+    def waitable_step(fun):
+        def wrapper(*args, **kwargs):
+            args[0].espaciado()
+            fun(args[0])
+            args[0].espaciado_final()
+            args[0].esperar()
+        return wrapper
+
+
+    @waitable_step
     def revision_estudio(self):
-        self.espaciado()
         print("-> Abrir Gantt de estudio")
         #TODO: Action: abrir archivo de gantt estudio pdf y herramienta de gantt online
         print("-> Revisar los topicos activos del proyecto en gantt")
@@ -27,46 +41,45 @@ class Jobs:
         print("-> Ajusta los tiempos necesarios")
         print("-> Incorpora o elimina topicos segun las necesidades de estudio en td para el proyecto")
         #TODO: Action: Abrir lista de necesidades de estudio en situacion actual
-        self.espaciado_final()
     
+    @waitable_step
     def revision_emigracion(self):
-        self.espaciado()
         print("Revision de plan Emigracion!")
         #TODO: Action: Abrir el archivo de plan inmigracion en excalidraw local (intentar hacer que se abra cargando el archivo automaticamente)
         print("Revisar diagrama del plan y estimar la evolucion")
         print("Actualizar el estado tanto de las acciones/proyectos como el diagrama")
         #TODO: ACTION: Listar acciones por proyecto activo
-        self.espaciado_final()
+        
     
+    @waitable_step
     def revision_algun_dia_tal_vez(self):
-        self.espaciado()
         print("-> Lista los proyectos y determina cuales son activos activos...") 
         #TODO: action: listar proyectos
         #TODO Permitir al usuario seleccionar en algun lugar los proyectos activos
         print("-> Lista incubables relacionados a esos proyectos")
         #TODO: abrir instancia en tmux con el comando now una vez por cada proyecto marcado como activo
         print("-> Ir determinando si hay que desincubar algo...")
-        self.espaciado_final()
+        
 
+    @waitable_step
     def delegados(self):
-        self.espaciado()
         print("Revisa lista de delegados y determina que hacer")
         #TODO: action: Abrir lista de delegados
         print("No introduzcas en inbox, directamente a siguiente accion o calendario")
         #TODO: abrir calendario calcurse
         #TODO: Abrir $HOME con el comando TD ADD de manera iterativa
-        self.espaciado_final()
+        
 
+    @waitable_step
     def recopilar(self):
-        self.espaciado()
         print("Recopila TODO los inputs e ideas a tus bandejas de entrada")
         print("En caso de no poder recopilar algo, deja un recordatorio de ello en la bandeja")
         #TODO: ACTION: Abrir fuentes de informacion digitales una por vez
         #TODO: ACTION: Abrir el directorio de "input" con el comando TD ADD de manera iteractiva
-        self.espaciado_final()
+        
 
+    @waitable_step
     def revisar_calendario(self):
-        self.espaciado()
         print("-> Revisa tu calendario de la semana vivida")
         #TODO: ACTION: Abrir calcurse posicionado a comienzo de semana
         print("")
@@ -75,20 +88,20 @@ class Jobs:
         print("  Olvidaste algun compromiso?")
         print("  Algo no pudo hacerse y debe reprogramarse?")
         print("  Algo se tuvo que cancelar?")
-        self.espaciado_final()
+        
     
+    @waitable_step
     def revisar_acciones_de_proyectos_mas_importantes(self):
-        self.espaciado()
         print("Que acciones realice? Modificar el estado de las ya hechas o canceladas")
         #TODO: ACCION: Listar acciones por proyecto de manera interactiva una lista de acciones por vez|
         print("Ahora mira los proyectos MAS importantes y...")
         #TODO: ACCION: Listar acciones por proyecto de manera interactiva una lista de acciones por vez|
         print("Que acciones traban otras de gran impacto? Puedo priorizarlas de alguna manera?")
         #TODO: ACCION: Listar acciones por proyecto de manera interactiva una lista de acciones por vez|
-        self.espaciado_final()
+        
     
+    @waitable_step
     def actualizar_estado_de_bloqueo_de_acciones(self):
-        self.espaciado()
         print("Por cada proyecto importante actualmente")
         print("Revisa que ACCIONES ya NO estan mas BLOQUEADAS e indicalo")
         #TODO: ACCION: listar proyectos
@@ -103,10 +116,9 @@ class Jobs:
         #TODO: ACCION: listar proyectos
         #TODO: ACCION: listar acciones por cada proyecto de manera interactiva 1 c/vez
         #TODO: ACCION: Pausa
-        self.espaciado_final()
     
+    @waitable_step
     def revisar_situacion_actual(self):
-        self.espaciado()
         print("-> Determina los items importantes de SITUACION ACTUAL")
         #TODO: Listar items SIN REPETICION de situacion actual
         #TODO: ACCION: Permitir escojer los items de situacion actual a usar
@@ -120,10 +132,10 @@ class Jobs:
         print("-  -> Abrir lista de projects importantes y responder")
         #TODO: action: Abrir lista de proyectos
         print("      ¿Deberia invertir algun recurso en algun proyecto o accion prioritaria?")
-        self.espaciado_final()
+        
 
+    @waitable_step
     def actualizar_situacion_actual(self):
-        self.espaciado()
         #TODO: ACTION: por cada lista de fuente de informacion actual listar una a una y realizar las actions siguientes
         #TODO: Action: Abrir el directorio de situacion actual usando now
         print("-> Abri el directorio de situacion actual con now")
@@ -136,22 +148,21 @@ class Jobs:
         print("Financiera: Banco patagonia homebanking, Santander homebanking, AirTM app, MercadoPago app, Banco Nacion App, Sube App")
         print("SERVICIOS de casas: www.naturgy.com.ar, edenordigital.com:Edenor1Lola y Aysa solo va por correo")
         print("Conocimiento: Notas en papel, Zettelkasten")
-        self.espaciado_final()
+        
 
+    @waitable_step
     def revisar_calendario_siguiente_semana(self):
-        self.espaciado()
         print("Abri el calendario")
         self._system.open_cal()
         print("Revisa la semana siguiente")
         print("")
         print("-> Buscar que compromisos voy a tener")
         print("-> Procesar: ¿Que deberia hacer para maximizar el exito de esos compromisos?")
-        self.esperar()
-        self._system.close_cal()
+        #FIX: cerrar calendario despues de esperar el paso
 
+    @waitable_step
     def registro_revision(self):
         #TODO: El registro puede hacerse digitalmente de manera programatica siguiendo las respuestas del usuario, hacerlo de manera programatica
-        self.espaciado()
         print("Considerando el valor de impacto de los objetivos de medio plazo")
         print("")
         print("-> Abra el registro de revisiones en ~/registro_revisiones/revisiones.md")
@@ -164,57 +175,42 @@ class Jobs:
         print("")
         print("- Dado que para el proceso de planificacion se usan herramientas como excalidraw,")
         print("-> debe volcar esos archivos con un nombre apropiado")
-        self.espaciado_final()
+        
 
-    def esperar(self):
-        input("Terminaste? s")
-
+    @waitable_step
 def fin_revision_futuro():
     j= Jobs()
     print("Lo lograste! Revision semanal futuro completada")
     os.system("clear")
     print("Ahora, vuelque tanto las decisiones tomadas como el metodo elegido para cada una.")
-    j.esperar()
+    
 
 def run_short_future_revision():
-    j= Jobs()
+    j= Jobs(system=system())
     j.registro_revision()
-    j.esperar()
     j.revisar_calendario()
-    j.esperar()
+    #TODO: improve design, tools used for steps should only be managed into step
     j.revisar_calendario_siguiente_semana()
-    j.esperar()
     fin_revision_futuro()
 
 def run_long_future_revision():
     j= Jobs()
     j.registro_revision()
-    j.esperar()
     j.revisar_calendario()
-    j.esperar()
     j.revisar_calendario_siguiente_semana()
-    j.esperar()
     j.revisar_situacion_actual()
-    j.esperar()
     j.revision_algun_dia_tal_vez()
-    j.esperar()
     j.revision_estudio()
-    j.esperar()
     fin_revision_futuro()
 
 
 def run_past_revision():
     j= Jobs()
     j.actualizar_estado_de_bloqueo_de_acciones()
-    j.esperar()
     j.revisar_acciones_de_proyectos_mas_importantes()
-    j.esperar()
     j.delegados()
-    j.esperar()
     j.recopilar()
-    j.esperar()
     j.actualizar_situacion_actual()
-    j.esperar()
     print("Lo lograste! Revision semanal pasado completada")
 
 #TODO: Las acciones DEBEN ser efectuadas en una instancia de terminal una separada de otra, usando tmux para multiplexarla
